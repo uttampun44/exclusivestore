64fabd985cebfa21b6c0b548cd7e9238dded9096
uttam pun <uttampun62@gmail.com>
2023-10-04 09:21:24 +0545

    image showing form nodejs routes fixed

--- exclusiveclient/src/components/homepage/Home.jsx
+++ exclusiveclient/src/components/homepage/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
    ]
     const fethProduct = async () =>{
         try {
-             const response = await fetch('/products', {
+             const response = await fetch('/api/products', {
                 method: 'GET',
                 mode: 'cors',
                 headers: {
@@ -35,7 +35,7 @@ function Home() {
 
              const data = await response.json();
 
-             console.log(data)
+             console.log(data.products)
              if (response.status === 200) {
               setProducts(data.products);
             }
@@ -89,45 +89,54 @@ function Home() {
 
                        {/* flash sales */}
                        <div className='flash_sales_cantainer mt-6 mb-10'>
-                           <div className='flash_sales_row flex gap-[4rem]'>
+                           <div className='flash_sales_row flex gap-[4rem] mobile:gap-9'>
 
                            <div className='flash_sales_title self-end'>
-                               <h1 className='text-secondary font-primary text-2xl font-semibold leading-10'>Flash Sales</h1>
+                               <h1 className='text-secondary font-primary text-2xl font-semibold leading-10 mobile:text-lg'>Flash Sales</h1>
                            </div>
                            <div className='days_limit_offer flex gap-4'>
                                <div className='offer_days'>
-                                   <p className='text-secondary font-secondary text-xs leading-5 font-medium'>Days</p>
-                                   <p className='text-secondary font-primary text-3xl font-bold leading-8'>03 <span>:</span></p>
+                                   <p className='text-secondary font-secondary text-xs leading-5 font-medium '>Days</p>
+                                   <p className='text-secondary font-primary text-3xl font-bold leading-8 mobile:flex mobile:text-lg'>03 <span>:</span></p>
                                </div>
                                <div className='offer_hours'>
                                  <p className='text-secondary font-secondary text-xs leading-5 font-medium'>Hours</p>
-                                 <p className='text-secondary font-primary text-3xl font-bold leading-8'>04  <span>:</span></p>
+                                 <p className='text-secondary font-primary text-3xl font-bold leading-8 mobile:flex mobile:text-lg'>04  <span>:</span></p>
                                </div>
                                <div className='offer_minutes'>
                                   <p className='text-secondary font-secondary text-xs leading-5 font-medium'>Minutes</p>
-                                  <p className='text-secondary font-primary text-3xl font-bold leading-8'>30 <span>:</span></p>
+                                  <p className='text-secondary font-primary text-3xl font-bold leading-8 mobile:flex mobile:text-lg'>30 <span>:</span></p>
                                </div>
                                <div className='offer_seconds'>
                                    <p className='text-secondary font-secondary text-xs leading-5 font-medium'>Seconds</p>
-                                   <p className='text-secondary font-primary text-3xl font-bold leading-8'>03</p>
+                                   <p className='text-secondary font-primary text-3xl font-bold leading-8 mobile:text-lg'>03</p>
                                </div>
                            </div>
                            </div>
                        </div>
 
                        {/* product carousel */}
-                       <div className='product_gallery flex justify-between items-center mb-14'>
+                       <div className='product_gallery flex justify-between items-center mb-14 tablet:flex-wrap mobile:flex-wrap mobile:row-y-4'>
                              {
                                 products.slice(0, 5).map((product) =>{
                                   return(
-                                    <div className='products grid gap-y-2' key={product.id} >
-                                      <img src={product.image} alt='product_image'/>
-                                         <p className='product_name font-secondary text-base leading-5 font-semibold'>{product.productName}</p>
+                                    <div className='products grid mobile:gap-y-4 mobile:w-[100%] pr-4 gap-y-4' key={product.id} >
+                                      <img src={product.image} alt='product_image' className='w-[146px] h-[140px] object-contain'/>
+                                       <div className='product_price'>
+                                       <p className='product_name font-secondary text-base leading-5 font-semibold'>{product.productName}</p>
                                          <p className='font-secondary text-[#DB4444]'>$ {product.price}</p>
+                                       </div>
+
+                                       <div className='add_to_cart'>
+                                           <button className='bg-black w-[100%] text-primary py-2 font-secondary leading-6 font-medium text-base rounded'>Add To Cart</button>
+                                       </div>
                                     </div>
                                   )
                                 })
                              }
+                       </div>
+                       <div className='view_products flex justify-center my-16'>
+                                  <Link to='/products' className='bg-[#DB4444] py-4 text-base font-secondary leading-6 px-12 font-medium text-white'>View All Products</Link>
                        </div>
                  </div>
             </section>
--- exclusiveclient/vite.config.js
+++ exclusiveclient/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   "server":{
     port: '3000',
     proxy: {
-      '/products':{
+      '/api/products':{
         target: 'http://localhost:8080/',
         changeOrigin: true,
       }
